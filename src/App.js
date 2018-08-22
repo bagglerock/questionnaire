4948a58d5fbcd03947bc5116d883484bb7615f3c
Oscar Villalta <bagglerock@gmail.com>
2018-08-22 14:53:21 -0400

    added the json to the app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { Jumbotron } from "./components/Jumbotron"; 
 import { Title } from "./components/Title";
 
+import qa from "./qa.json";
+
 import "./App.css";
 
+console.log(qa);
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
