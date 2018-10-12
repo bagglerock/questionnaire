f42493323e4c959ace5f54e92b671be9ed95d26a
Oscar Villalta <bagglerock@gmail.com>
2018-10-12 03:43:16 -0400

    npm init again

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -68,21 +68,27 @@ module.exports = {
       path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
   },
   resolve: {
-    root: [
-        __dirname,
-        path.resolve(__dirname, "app/components"),
-        path.resolve(__dirname, "app/api")
-    ],
-    modulesDirectories: [ 'node_modules' ],
+    // This allows you to set a fallback for where Webpack should look for modules.
+    // We placed these paths second because we want `node_modules` to "win"
+    // if there are any conflicts. This matches Node resolution mechanism.
+    // https://github.com/facebookincubator/create-react-app/issues/253
+    modules: ['node_modules', paths.appNodeModules].concat(
+      // It is guaranteed to exist because we tweak it in `env.js`
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    ),
+    // These are the reasonable defaults supported by the Node ecosystem.
+    // We also include JSX as a common component filename extension to support
+    // some tools, although we do not recommend using it, see:
+    // https://github.com/facebookincubator/create-react-app/issues/290
+    // `web` extension prefixes have been added for better support
+    // for React Native Web.
+    extensions: ['.web.js','.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-        app: 'app',
-        applicationStyles: 'app/styles/app.scss',
-        actions: 'app/actions/actions.jsx',
-        reducers: 'app/reducers/reducers.jsx',
-        configureStore: 'app/store/configureStore.jsx'
+      
+      // Support React Native Web
+      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      'react-native': 'react-native-web',
     },
-    extensions: ['', '.js', '.jsx']
-},
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
       // This often causes confusion because we only process files within src/ with babel.
