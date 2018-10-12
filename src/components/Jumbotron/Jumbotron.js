ad0ee282ccc7f99f0095e6c193b1cc593d2c4855
Oscar Villalta <bagglerock@gmail.com>
2018-10-12 01:25:58 -0400

    learned some bootstrap 4+ features... order... no more xs...

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -6,31 +6,37 @@ export const Jumbotron = props => {
 
   return (
     <div className="jumbotron">
-      <div className="navigator">
-        <Arrow 
-            direction="left"
-            action={props.decrementNumber}
-        />
-        <Arrow 
-            direction="right"
-            action={props.incrementNumber}
-        />
-      </div>
-      <div className="body">
-        <h1 className="display-4">Question {props.questionNumber}</h1>
-        <p className="lead">{props.question.question}</p>
-        <hr className="my-4" />
+      <div className="container">
+        <div className="row">
+            <div className="col-3">
+                <Arrow 
+                    direction="left"
+                    action={props.decrementNumber}
+                />
+            </div>
+            <div className="col-6">
+                <h1 className="display-5">Question {props.questionNumber}</h1>
+                <p className="lead">{props.question.question}</p>
+                <hr className="my-4" />
 
-        {props.question.answers.map((answer, i) => (
-          <Button 
-            key={i} 
-            action={props.chooseAnswer}
-            index={i}
-          >{answer.choice}
-          </Button>
-        ))}
-
+                {props.question.answers.map((answer, i) => (
+                <Button 
+                    key={i} 
+                    action={props.chooseAnswer}
+                    index={i}
+                >{answer.choice}
+                </Button>
+                ))}
+            </div>
+            <div className="col-3">
+                <Arrow 
+                    direction="right"
+                    action={props.incrementNumber}
+                />
+            </div>
+        </div>
       </div>
+
     </div>
   );
 };
