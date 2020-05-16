96656b2db5942310c871abd979c3ce9fb9a6f271
Oscar Villalta <bagglerock@gmail.com>
2020-05-16 01:18:51 -0400

    Updates

---
+++ precache-manifest.bb07525ea93903ee80843a3157228707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d573b4c1a727147df1afa04ee8ff5ac3",
+    "url": "/trivia-react/index.html"
+  },
+  {
+    "revision": "f15335d60cc121cf7136",
+    "url": "/trivia-react/static/css/main.03f20225.chunk.css"
+  },
+  {
+    "revision": "8354b01e14b2b1fdc8c0",
+    "url": "/trivia-react/static/js/2.486c2b32.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trivia-react/static/js/2.486c2b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15335d60cc121cf7136",
+    "url": "/trivia-react/static/js/main.509f3ff3.chunk.js"
+  },
+  {
+    "revision": "6f7c215137a527e95c36",
+    "url": "/trivia-react/static/js/runtime-main.d3ac2cdd.js"
+  }
+]);
