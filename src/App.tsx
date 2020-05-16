6b3632742a9dbad19dd7459f43b695602a341fa9
Oscar Villalta <bagglerock@gmail.com>
2020-05-16 01:17:55 -0400

    feat: add bootstrap packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 
 export const App: React.FC = () => (
-  <div className="App">
+  <div className="bg-danger">
     <h1>Weird Trivia Fun</h1>
-    <div>
+    <Jumbotron className="mb-5">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi modi
-        minima, suscipit expedita nobis sunt neque temporibus. Sed asperiores
-        voluptate tempora, nesciunt, dolorum officia saepe id, in qui rem
-        accusamus!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi modi minima, suscipit expedita nobis sunt neque temporibus. Sed
+        asperiores voluptate tempora, nesciunt, dolorum officia saepe id, in qui rem accusamus!
       </p>
-    </div>
+    </Jumbotron>
     <footer>&copy; Oscar Villalta</footer>
   </div>
 );
 
 // will need a game state, maybe in context
 // will need something to control the timers, maybe a hook
+
+// try to hook this to some API to get questions
+// have to get bootstrap working
