4cadd9a6131bc3eacfb2c09f483abcc27a60b3a4
Oscar Villalta <bagglerock@gmail.com>
2020-05-16 00:52:37 -0400

    feat: start app from scratch in typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const App: React.FC = () => (
+  <div className="App">
+    <h1>Weird Trivia Fun</h1>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi modi
+        minima, suscipit expedita nobis sunt neque temporibus. Sed asperiores
+        voluptate tempora, nesciunt, dolorum officia saepe id, in qui rem
+        accusamus!
+      </p>
+    </div>
+    <footer>&copy; Oscar Villalta</footer>
+  </div>
+);
+
+// will need a game state, maybe in context
+// will need something to control the timers, maybe a hook
