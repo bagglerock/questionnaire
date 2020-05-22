88db2943e6b18435031f9e5480404d7c4da01337
Oscar Villalta <bagglerock@gmail.com>
2020-05-21 23:48:06 -0400

    feat: add some responsiveness and styling

--- src/sections/Main/Answer.tsx
+++ src/sections/Main/Answer.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export const Answer: React.FC = () => (
   <>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi modi minima, suscipit expedita nobis sunt neque temporibus. Sed
-      asperiores voluptate tempora, nesciunt, dolorum officia saepe id, in qui rem accusamus! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Repudiandae error, sed sint pariatur ratione similique voluptas minima. Reiciendis voluptates quia facere a illo
-      exercitationem voluptatem recusandae tempora repellendus corporis vero adipisci architecto assumenda, quibusdam quasi quisquam nobis
-      facilis quos et similique quas dignissimos, ut quaerat? Et laboriosam at animi dignissimos sapiente magni repellendus. Veniam, eum!
-      Corporis totam suscipit, perspiciatis, voluptates earum dolor est deleniti iusto, explicabo deserunt quae magni voluptatem dolorum
-      illum aspernatur alias? Harum, suscipit nulla culpa ipsum consequatur, possimus aliquam, ipsa at dicta laudantium aperiam? Ratione
-      nostrum exercitationem natus dolor quo facilis distinctio laudantium provident laborum odit earum quidem autem, debitis deserunt
-      alias. Consectetur officiis perspiciatis tenetur alias id, facilis natus totam dolores, vel voluptatibus est nam quod tempore! Nobis
-      asperiores rem esse quod cum doloribus officia deleniti, at quis vitae obcaecati consequatur optio temporibus ea labore amet illo?
-      Commodi eos nemo, iusto debitis optio reprehenderit aperiam voluptate. Incidunt ipsum cum numquam nemo debitis libero iusto hic
-      expedita. Vero, quam, placeat consectetur cumque harum assumenda quibusdam quia veniam nesciunt deserunt nam nisi non fugiat, error
-      rem, nisi delectus laborum aut nostrum dolorum ullam alias. Atque accusamus illum porro corporis corrupti aperiam in vero, aut
-      quaerat. Consequuntur, amet dolorem sapiente nostrum, fuga earum provident eum numquam quae, modi possimus impedit. Consequuntur
-      molestias odio officiis sit, placeat iure facilis magnam ab, illum repellat, amet voluptatibus sed quod architecto. Quasi eligendi
-      suscipit enim quas explicabo non! Reprehenderit temporibus amet eum labore. Harum hic, assumenda nihil fugit quas, iste iusto debitis
-      ipsam non esse accusantium recusandae ab reprehenderit. Vel rerum facere quia animi officiis vitae blanditiis necessitatibus
-      asperiores quo inventore sunt quos modi natus molestias, laudantium magnam eum ab exercitationem atque. Exercitationem corporis aut
-      obcaecati soluta doloremque, cumque quae iste non delectus deserunt fugiat eligendi architecto voluptatem tempore aliquam expedita hic
-      cupiditate eum officia vero quod? Ducimus, porro nobis harum, tenetur sit dolorem sunt voluptate excepturi earum fugiat praesentium
-      voluptas molestiae distinctio doloremque, animi placeat provident. Eveniet harum perferendis ipsum ut exercitationem deleniti est id
-      odio, veritatis suscipit molestias mollitia iusto repellat aliquid culpa ratione! Facere doloremque minima eum earum dolorem maxime
-      nostrum voluptatibus cum minus sapiente hic id sint autem in fugiat mollitia nesciunt odit eveniet, quod deleniti nemo quis nisi.
-      Pariatur quasi, esse quos vero recusandae incidunt quod alias asperiores aperiam fuga?
-    </p>
+    <Button>Answer</Button>
+
+    <Button>Answer</Button>
+
+    <Button>Answer</Button>
+
+    <Button>Answer</Button>
   </>
 );
--- src/sections/Main/Question.tsx
+++ src/sections/Main/Question.tsx
@@ -4,10 +4,10 @@ import { Jumbotron } from 'react-bootstrap';
 export const Question: React.FC = () => (
   <>
     <Jumbotron>
-      <p>
+      <h2>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nulla, quidem rem consequatur temporibus, sed facilis, aut minus
         animi nostrum iusto voluptatum cumque. Odio excepturi eaque enim quos! Inventore, repellat?
-      </p>
+      </h2>
     </Jumbotron>
   </>
 );
